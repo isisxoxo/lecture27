c0b21e53c27880402aa076c216b0de58895b4155
Isis <isislee107@gmail.com>
2024-04-23 10:36:11 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/lecture27/Lecture27ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.lecture27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
